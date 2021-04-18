2c78c60a8bbb8530a6b2f31ea0ca9e8a2aa42d69
aaratee-123 <82765818+aaratee-123@users.noreply.github.com>
2021-04-18 20:01:08 +0530

    Add files via upload

---
+++ write a program  for chatting.py
@@ -0,0 +1,33 @@
+print("open your whats up")
+friend=input("enter the name")
+if friend=="ashwini":
+    print("Hi! ashwini")
+    school=input("enter the school name")
+    if school=="zpschool":
+        print("yes!yesterday")
+        say=input("enter answer yes or no")
+        if say=="yes":
+            print("Which lesson teacher teach?")
+            english=input("enter the topic")
+            if english=="grammer":
+                print("Which type of grammer?")
+                grammer=input("enter the type of grammer")
+                if grammer=="tenses":
+                    print("ok,thanks for giving information")
+                    say=input("enter the word")
+                    if say=="thanks":
+                        print("most welcome!")
+                    else:
+                        print("it's my pleasure")
+                else:
+                    print("okay!fine")
+            else:
+                print("which type of question gives teacher")
+        else:
+            print("maths x,y-axis question")
+    else:
+        print("yes,but for half day")
+else:
+    print("hellow!geeta ")
+                
+                
