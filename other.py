b3850c25511a7aaf1fd6f47ea0e06e83274becbf
aaratee-123 <82765818+aaratee-123@users.noreply.github.com>
2021-04-18 19:55:34 +0530

    Add files via upload

---
+++ other.py
@@ -0,0 +1,177 @@
+# 1) write a program to calculate avarage
+# i=1
+# sum=0
+# while i<=10:
+#     num=int(input("enter the number"))
+#     sum=sum+num
+#     i=i+1
+# print(sum)
+# avarage=sum/10
+# print(avarage)
+
+#2) write print  multiplication table
+# i=1
+# while i<=10:
+#     print(24*i)
+#     i=i+1
+
+# 3)write a program infinite loop
+# i=1
+# while i<=5:
+#     print(i)
+# i=i+1
+
+# 4)write a program factorial of any number
+# i=int(input("enter the number"))
+# fact=1
+# while i>0:
+#     fact=fact*i
+#     i=i-1
+# print("factorial",fact)
+
+#5) write a program sum of digits of number
+# i=1
+# sum=0
+# num=int(input("enter the number"))
+# while i<=num:
+#     a=(num%10)
+#     sum=sum+a
+#     num=num//10
+# print(sum)
+
+# 6) write a program to find armstrong number
+# i=1
+# sum=0
+# num=int(input("enter the number"))
+# temp=num
+# while i<=num:
+#         a=(num%10)
+#         sum=sum+a**3
+#         num=num//10
+# print(sum)
+# if temp==sum:
+#     print("it is armstrong number") 
+# else:
+#     print("it is not armstrong number")
+
+# 7) write a program to find reverse number
+# i=1
+# rev=0
+# num=int(input("enter the number"))
+# while i<=num:
+#     a=num%10
+#     rev=rev*10+a
+#     num=num//10
+# print(rev)
+
+# 8) write a program to find prime factor
+# n=int(input("enter the number"))
+# i=2
+# while (n!=1):
+#     rem=n%i
+#     if rem==0:
+#         n=n/i
+#         print(i)
+#     else:
+#         i=i+1
+
+# 9)write a program to find prime number
+# n=int(input("enter the number"))
+# temp=n
+# i=1
+# while n!=i:
+#     if n%temp==0:
+#         if n%2!=0 and n%3!=0:
+#             print("it")
+#         else:
+#             print("it is not prime number")
+#         if i==1:
+#             break
+#         i=i+1
+#10) print pattern
+# i=1
+# while i<=4:
+#     print("*"*i)
+#     i=i+1
+
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+i=1
+while i<=100:
+    if (a%i==0) and (b%i==0):
+print(i)
+    i=i+1
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+    
+
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
